9b6d9056e444f8d2b53b02e702f1028ee55fe069
kokoro2580 <hatsakon5806@gmail.com>
2026-01-30 22:33:21 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 void main() {
-    Character jed = new Character(99 ,51);
+    Character jed = new Character(100 ,50);
     jed.swingSword();
     jed.swingSword();
     jed.swingSword();
     jed.swingSword();
     jed.swingSword();
-    jed.takeDamage(30);
+    jed.takeDamage(-30);
     jed.takeDamage(50);
     jed.takeDamage(20);
     jed.rest();
